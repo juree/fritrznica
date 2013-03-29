2ba0c4c8b5b486046a9a3c6db8a9e1ad8dc255f7
primoz143 <primoz.karnicnik@gmail.com>
2013-03-29 15:31:00 +0100

    petekZadnji

--- fritrznica/trznica/views.py
+++ fritrznica/trznica/views.py
@@ -4,9 +4,16 @@ from django.template import RequestContext
 from django.http import HttpResponseRedirect
 
 def trznica_main(request):
+    ime="Primoz"
+    vpisna="63110367"
+    priimek="Karnicnik"
+    predmet= "IPRI"
+    termin1= "PON 7:30- 9:00"
+    termin2="PET 7:30-9:00"
+    space= " "
     if request.user.is_authenticated():
         if request.user.has_perm('fritrznica.activated'):
-            return render_to_response('trznica.html', RequestContext(request))
+            return render_to_response('trznica.html', RequestContext(request,{'termin1': termin1, 'ime': ime, 'priimek': priimek, 'space':space, 'predmet':predmet,'vpisna':vpisna, 'termin2':termin2}))
         else:
             return HttpResponseRedirect("/aktivacija/")
     else:
