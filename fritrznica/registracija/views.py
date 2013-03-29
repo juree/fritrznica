5cb04b36fe44cea0ed030069c66ed10fc2b3220e
Nejc <nejc.il92@gmail.com>
2013-03-29 09:48:26 +0100

    url aktivacija fixed, trznica login required

--- fritrznica/registracija/views.py
+++ fritrznica/registracija/views.py
@@ -5,18 +5,18 @@ from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect
 
 def user_register(request):
-    username = password = email = firstname = lastname = state = ''
+    username = password = firstname = lastname = state = ''
     if request.POST:
         username = request.REQUEST.get('username', None)
         password = request.REQUEST.get('password', None)
-        email = request.REQUEST.get('email', None)
+        #email = request.REQUEST.get('email', None)
         firstname = request.REQUEST.get('firstname', None)
         lastname = request.REQUEST.get('lastname', None)
 
         # TODO: check if already existed
 
-        if username and password and email and firstname and lastname:
-            user = User.objects.create_user(username, email, password)
+        if username and password and firstname and lastname and not User.objects.filter(username=username).count() > 0:
+            user = User.objects.create_user(username, password)
             user.first_name = firstname
             user.last_name = lastname
             user.save()
--- fritrznica/trznica/views.py
+++ fritrznica/trznica/views.py
@@ -1,9 +1,13 @@
 # Create your views here.
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from django.http import HttpResponseRedirect
 
 def trznica_main(request):
     if request.user.has_perm('fritrznica.activated'):
-        return render_to_response('trznica.html', RequestContext(request))
+        if request.user.is_authenticated():
+            return render_to_response('trznica.html', RequestContext(request))
+        else:
+            return HttpResponseRedirect("/prva/")
     else:
-        return render_to_response('activate.html', RequestContext(request))
+        return HttpResponseRedirect("/aktivacija/")
