156de9d181abdd6d14affab2ca69d0083a686d6d
Jure Prevc <jure.prevc@gmail.com>
2013-05-13 21:39:31 +0200

    offerSwap init.

--- fritrznica/fritrznica/urls.py
+++ fritrznica/fritrznica/urls.py
@@ -30,6 +30,7 @@ urlpatterns = patterns('',
                        (r'^firstFromUcilnica/$', include('firstFromUcilnica.urls')),
                        (r'^profil/$','firstFromUcilnica.views.profil'),
                        (r'^offer/$','offer.views.offer'),
+                       (r'^offer/(?P<id>\d+)/$','offer.views.offerSwap'),
                        (r'^api/',include(v1_api.urls)),
 
                        # (r'^registracija/$', 'registracija.views.user_register'),
