f4ab0fbe4675ec62dadfe63366e1128d60945e86
Nejc <nejc.il92@gmail.com>
2013-05-14 00:10:20 +0200

    Filtering works on parsedoffers and offers, always latest version working (finally)

--- fritrznica/static/angular/services.js
+++ fritrznica/static/angular/services.js
@@ -1,13 +1,13 @@
 var app = angular.module('ipriServices', ['ngResource']);
 
 app.factory('Offer', function($resource){
-    return $resource('/api/v1/offers', {}, {
+    return $resource('/api/v1/offers?version='+VERSION, {}, {
         query: {method:'GET', isArray:false}
     });
 });
 
 app.factory('Parsedoffer', function($resource){
-    return $resource('/api/v1/parsedoffers', {}, {
+    return $resource('/api/v1/parsedoffers?user='+USER_ID+'&version='+VERSION+'&offered=false', {}, {
         query: {method:'GET', isArray:false}
     });
 });
